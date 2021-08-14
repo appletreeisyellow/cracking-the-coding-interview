d924934face49adbd35202fb90694ca7e6739f1e
Chunchun (Vivo) <yccvivo@gmail.com>
2021-08-14 13:54:42 -0700

    add 1.5

---
+++ ch01_arrays-and-strings/1.5_one-away.py
@@ -0,0 +1,65 @@
+import unittest
+
+def is_one_away(str1, str2):
+  len1 = len(str1)
+  len2 = len(str2)
+  if len1 == len2:
+    return is_one_char_replaceable(str1, str2)
+  elif len1 + 1 == len2:
+    return is_one_char_insertable(str1, str2)
+  elif len2 + 1 == len1:
+    return is_one_char_insertable(str2, str1)
+  else:
+    return False
+
+def is_one_char_replaceable(str1, str2):
+  found_diff = False
+  for i in range(len(str1)):
+    if str1[i] != str2[i]:
+      if found_diff:
+        return False
+      found_diff = True
+  return True
+
+def is_one_char_insertable(str1, str2):
+  # assume length of str1 < str2
+  index1 = 0
+  index2 = 0
+  while index1 < len(str1) and index2 < len(str2):
+    if str1[index1] != str2[index2]:
+      if index1 != index2:
+        return False
+      index2 += 1
+    else:
+      index1 += 1
+      index2 += 1
+  return True
+
+class Test(unittest.TestCase):
+  test_cases = [
+    # add test cases here, ("test-case", True)
+    ("pale", "ple", True),
+    ("pales" ,"pale", True),
+    ("pale" ,"pales", True),
+    ("pale", "bale", True),
+    ("pale", "bae", False),
+    ("pale", "palesd", False),
+    ("pale", "btle", False),
+  ]
+  test_functions = [
+    # add testing functions here
+    is_one_away,
+  ]
+
+  def test(self):
+    for text1, text2, expected in self.test_cases:
+      for test_func in self.test_functions:
+        try:
+          assert (test_func(text1, text2) == expected), "Failed!"
+        except AssertionError as e:
+          e.args += (test_func.__name__, text1, text2, "should be " + str(expected))
+          raise
+
+
+if __name__ == "__main__":
+  unittest.main()
