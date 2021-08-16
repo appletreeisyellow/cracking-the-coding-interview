9aa04d9719238706a138eff35953c6ba90ea0fc2
Chunchun (Vivo) <yccvivo@gmail.com>
2021-08-16 10:20:28 -0700

    add question in 1.4

--- ch01_arrays-and-strings/1.4_palindrome_permutation.py
+++ ch01_arrays-and-strings/1.4_palindrome_permutation.py
@@ -1,5 +1,21 @@
 import unittest
 
+"""
+
+1.4 Palindrome Permutation
+
+  Given a string, write a function to check if it is
+  a permutation of a palindrome. A palindrome is a word
+  or phrase that is the same forwards and backwards. A
+  permutation is a rearrangement of letters. The palindrome
+  does not need to be limited to just dictionary words.
+
+  EXAMPLE
+  Input:   Tact Coa
+  Output:  True (permutation: "taco cat", 'atco cta', etc)
+  
+"""
+
 def is_palindrome_permutation(string):
   # a palindrome is a string has at most one character that is odd
   
