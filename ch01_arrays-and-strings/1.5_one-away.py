625518dec155a62f061a73a946e6aedd1fac92ee
Chunchun (Vivo) <yccvivo@gmail.com>
2021-08-16 14:01:53 -0700

    add 1.5

--- ch01_arrays-and-strings/1.5_one-away.py
+++ ch01_arrays-and-strings/1.5_one-away.py
@@ -1,5 +1,22 @@
 import unittest
 
+"""
+
+1.5 One Away
+
+  There are three types of edits that can be performed on
+  strings: insert a character, remove a character, or
+  replace a character. Given two strings, write a function
+  to check if they are one edit (or zero edits) away.
+
+  EXAMPLE
+  pale, ple   -> true
+  pales, pale -> true
+  pale, bale  -> true
+  pale, bae   -> false
+
+"""
+
 def is_one_away(str1, str2):
   len1 = len(str1)
   len2 = len(str2)
