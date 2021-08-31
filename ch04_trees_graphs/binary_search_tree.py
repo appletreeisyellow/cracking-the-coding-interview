5ec1f5ce68d7410d7abdc7d0871e9c30f7ca08ee
Chunchun (Vivo) <yccvivo@gmail.com>
2021-08-31 14:51:21 -0700

    feat(binary_search_tree): add print_tree()

--- ch04_trees_graphs/binary_search_tree.py
+++ ch04_trees_graphs/binary_search_tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Node:
   def __init__(self, value):
     self.value = value
@@ -43,6 +45,23 @@ class BinarySearchTree:
         current = current.right
     raise Exception("No such value in the tree")
 
+  def print_tree(self):
+    # breath-first traversal
+    queue = deque()
+    queue.append((self.root, 0))
+    levels = []
+    while queue:
+      node, level = queue.popleft()
+      if len(levels) == level:
+        levels.append([])
+      levels[level].append(node.value)
+      if node.left:
+        queue.append((node.left, level + 1))
+      if node.right:
+        queue.append((node.right, level + 1))
+
+    for level in levels:
+      print(level)
 
 if __name__ == "__main__":
   bst = BinarySearchTree()
